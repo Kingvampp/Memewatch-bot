729c840c6f83bffcf0ab97adbbe5639809f2a73a
kingvampp <silvio.messi.delacruz@gmail.com>
2025-01-23 12:55:20 -0800

    Fix package initialization
    - Updated utils/__init__.py with proper imports
    - Added cogs/__init__.py package marker
    - Fixed import structure

---
+++ cogs/__init__.py
@@ -0,0 +1,3 @@
+"""Cogs package for the MemeWatch bot."""
+
+# This file can be empty, it just needs to exist to make the directory a package 
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,12 +1,17 @@
 """Utility package for the MemeWatch bot."""
+
+# Import the DatabaseManager class
 from .database import DatabaseManager
+
+# Import formatting functions
 from .formatting import (
-    format_number, 
-    format_price, 
+    format_number,
+    format_price,
     format_time_ago,
     format_percentage
 )
 
+# Define what should be available when importing from utils
 __all__ = [
     'DatabaseManager',
     'format_number',
